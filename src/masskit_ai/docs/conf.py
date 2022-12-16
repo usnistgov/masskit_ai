06965d4c897183be8ed0edf9261b72ac7cb24f28
lyg <lewis.geer@gmail.com>
2022-12-16 18:57:59 -0500

    - add license - add setup - fix sphinx setup - use nist pages template

--- src/masskit_ai/docs/conf.py
+++ src/masskit_ai/docs/conf.py
@@ -32,10 +32,12 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# sphinx.ext.githubpages turns off jekyll
 extensions = [
       'nbsphinx',
       'myst_parser',
-      'sphinx.ext.autodoc'
+      'sphinx.ext.autodoc',
+      'sphinx.ext.githubpages'
 ]
 
 source_suffix = {
@@ -47,18 +49,23 @@ source_suffix = {
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Suppress autodoc imports
+autodoc_mock_imports = ['pyarrow', 'rdkit', 'pandas', 'arrow', 'torch', 'masskit', 
+'pytorch_lightning', 'omegaconf', 'general', 'fingerprints', 'mlflow',
+'torchmetrics', 'hitlist', 'modules']
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'keras']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'classic'
+# nature is NIST compatible
+html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- src/masskit_ai/docs/index.rst
+++ src/masskit_ai/docs/index.rst
@@ -4,7 +4,7 @@
    contain the root `toctree` directive.
 
 Welcome to masskit_ai's documentation!
-====================================
+======================================
 
 .. toctree::
    :maxdepth: 4
