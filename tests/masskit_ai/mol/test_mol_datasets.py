55668946d3b2121cb5d519672eca46af8798f2d0
lyg <lewis.geer@nist.gov>
2023-02-13 16:48:50 -0500

    - update unit tests and data - update config for path files

--- tests/masskit_ai/mol/test_mol_datasets.py
+++ tests/masskit_ai/mol/test_mol_datasets.py
@@ -1,23 +1,26 @@
 import pytest
 from pytest import approx
+from masskit_ai.lightning import setup_datamodule
 from masskit_ai.mol.mol_datasets import MolPropDataset
 from hydra import compose, initialize
 from hydra.core.global_hydra import GlobalHydra
 import pytorch_lightning as pl
-from masskit_ai.spectrum.spectrum_lightning import MolDataModule, SpectrumDataModule, SpectrumLightningModule
+from masskit_ai.spectrum.spectrum_lightning import SpectrumDataModule, SpectrumLightningModule
 
 @pytest.fixture()
 def config_ri():
     GlobalHydra.instance().clear()
     initialize(config_path='.', version_base=None)
-    cfg = compose(config_name="config_ri") # , overrides=['input=2022_tandem_search_test'])
+    cfg = compose(config_name="config_ri", overrides=['input.train.spectral_library=data/mainlib_2017_path_trunc.parquet',
+        'input.train.where=null',
+        'input.valid.spectral_library=data/mainlib_2017_path_trunc.parquet',
+        'input.valid.where=null',
+    ])
     return cfg
 
 def test_MolPropDataset(config_ri):
-    ds = MolPropDataset('data/mainlib_2017_trunc.parquet', config_ri, 'train')
-    assert ds.get_data_row(0)['name'] == "Urea, N,N-dimethyl-N'-propyl-N'-octyl-"
-    ds2 = MolPropDataset('data/mainlib_2017_trunc.parquet', config_ri, 'train')
-    assert ds2.get_data_row(0)['name'] == "Urea, N,N-dimethyl-N'-propyl-N'-octyl-"
+    ds = MolPropDataset('data/mainlib_2017_path_trunc.parquet', config_ri, 'train')
+    assert ds.get_data_row(0)['name'] == "Valeramide, 5-chloro-N-propyl-N-octyl-"
 
 def test_SearchLightningModule(config_ri):
     model = SpectrumLightningModule(config_ri)
@@ -29,5 +32,5 @@ def test_SearchLightningModule(config_ri):
         limit_val_batches=9,
         log_every_n_steps=3,
     )
-    dm =  MolDataModule(config_ri) # MolDataModule
+    dm = setup_datamodule(config_ri) # MolDataModule
     trainer.fit(model, datamodule=dm)
