9a10f39d08717ade6b028f88e7873ffa4cf4daa5
lyg <lewis.geer@nist.gov>
2023-06-16 19:10:56 -0400

    - check airi output

--- tests/apps/ml/peptide/test_predict.py
+++ tests/apps/ml/peptide/test_predict.py
@@ -4,10 +4,10 @@ import os
 import re
 from masskit_ai.apps.ml.peptide import predict
 
-def test_predict_peptide(config_predict_peptide,create_predicted_peptide_arrow_file):
+def test_predict_peptide(config_predict_peptide):
     predict.main(config_predict_peptide)
-    assert os.path.exists(create_predicted_peptide_arrow_file[0])
-    with open(create_predicted_peptide_arrow_file[1]) as f:
+    assert os.path.exists(config_predict_peptide.predict.output_prefix+'.'+config_predict_peptide.predict.output_suffixes[0])
+    with open(config_predict_peptide.predict.output_prefix+'.'+config_predict_peptide.predict.output_suffixes[1]) as f:
         data = f.read()
         assert re.search(r'1915\.9524\t2\.46.*"y17\+i/0.0ppm"',data) is not None
         assert re.search(r'1817\.9044\t0\.11.*"y16\+i/0.0ppm"', data) is not None
@@ -15,4 +15,7 @@ def test_predict_peptide(config_predict_peptide,create_predicted_peptide_arrow_f
 
 def test_predict_airi(config_predict_airi):
     predict.main(config_predict_airi)
-#    return config_predict_airi.predict.output_prefix+"."+config_predict_airi.predict.output_suffixes[0]
+    with open(config_predict_airi.predict.output_prefix+'.'+config_predict_airi.predict.output_suffixes[0]) as f:
+        data = f.read()
+        assert re.search(r'1087\..*,21\..*',data) is not None
+        assert re.search(r'1640\..*,65\..*', data) is not None
