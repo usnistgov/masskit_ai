2d7dcefa36a3b2c4960f18bdafa208c3817f3485
lyg <lewis.geer@nist.gov>
2023-07-11 10:09:45 -0400

    - fix unit test for entry point

--- tests/apps/ml/peptide/test_predict.py
+++ tests/apps/ml/peptide/test_predict.py
@@ -5,7 +5,7 @@ from masskit_ai.apps.ml.peptide import predict
 
 
 def test_predict_peptide(config_predict_peptide):
-    predict.main(config_predict_peptide)
+    predict.predict_app(config_predict_peptide)
     assert os.path.exists(config_predict_peptide.predict.output_prefix+'.'+config_predict_peptide.predict.output_suffixes[0])
     with open(config_predict_peptide.predict.output_prefix+'.'+config_predict_peptide.predict.output_suffixes[1]) as f:
         data = f.read()
@@ -14,14 +14,14 @@ def test_predict_peptide(config_predict_peptide):
         assert data.count('Name:') == 6
 
 def test_predict_airi_smiles(config_predict_airi_smiles):
-    predict.main(config_predict_airi_smiles)
+    predict.predict_app(config_predict_airi_smiles)
     with open(config_predict_airi_smiles.predict.output_prefix+'.'+config_predict_airi_smiles.predict.output_suffixes[0]) as f:
         data = f.read()
         assert re.search(r'1525\..*,9\..*',data) is not None
         assert re.search(r'629\..*,31\..*', data) is not None
 
 def test_predict_airi_csv(config_predict_airi_csv):
-    predict.main(config_predict_airi_csv)
+    predict.predict_app(config_predict_airi_csv)
     with open(config_predict_airi_csv.predict.output_prefix+'.'+config_predict_airi_csv.predict.output_suffixes[0]) as f:
         data = f.read()
         assert re.search(r'1525\..*,9\..*',data) is not None
