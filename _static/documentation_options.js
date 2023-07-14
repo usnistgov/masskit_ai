006f084be59a9ce15766e8f3a7c6a0353f4f2373
lyg <lewis.geer@nist.gov>
2023-07-14 17:07:05 -0400

    new pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0-57-g7c937ec',
+    VERSION: '1.0.0-60-ge055069',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
