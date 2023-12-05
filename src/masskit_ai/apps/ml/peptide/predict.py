adc876f59861ed5d8feb52719a3c3a6c99c434a2
Douglas Slotta <douglas.slotta@nist.gov>
2023-12-05 16:42:15 -0500

    Kludge for pyarrow extension type deprication; version bump to 1.2.1

--- src/masskit_ai/apps/ml/peptide/predict.py
+++ src/masskit_ai/apps/ml/peptide/predict.py
@@ -4,6 +4,7 @@ import random
 
 import hydra
 import numpy as np
+import pyarrow as pa
 import pytorch_lightning as pl
 from hydra.core.plugins import Plugins
 from masskit.utils.general import MassKitSearchPathPlugin, class_for_name
@@ -26,6 +27,15 @@ except ImportError:
 
 @hydra.main(config_path="conf", config_name="config_predict", version_base=None)
 def predict_app(config):
+
+    # Quick fix for the following error message:
+    #
+    #   RuntimeWarning: pickle-based deserialization of pyarrow.PyExtensionType subclasses is disabled by default; 
+    #   if you only ingest trusted data files, you may re-enable this using `pyarrow.PyExtensionType.set_auto_load(True)`.
+    #   In the future, Python-defined extension subclasses should derive from pyarrow.ExtensionType 
+    #   (not pyarrow.PyExtensionType) and implement their own serialization mechanism.
+    # TODO: needs to be fixed right
+    pa.PyExtensionType.set_auto_load(True)
 
     with logging_redirect_tqdm():
 
